c665fe68df1cac1307eae3ec54677cdf8173a9b0
mihaifirst <varzaru.alexmihai@gmail.com>
2023-01-24 01:02:19 +0200

    update function

--- hobbies functions.js
+++ hobbies functions.js
@@ -1,3 +1,4 @@
+/*
 createHobbies("alergat")
 
 function createHobbies (ceva) {
@@ -5,4 +6,19 @@ function createHobbies (ceva) {
     hobbies.push(ceva)
     console.log(hobbies)
 }
+*/
+
+let hobbies = ["basket", "ceva", "volei"];
+
+updateHobbies("ceva" , "bowling");
+
+function updateHobbies(hobby, newHobby){
+    for(let i = 0; i < hobbies.length; i++){
+        if(hobbies[i] === hobby){
+            hobbies.splice(i,1, newHobby);
+        }
+    }
+}
+
+console.log(hobbies);
 
